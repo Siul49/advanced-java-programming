552bf69341ab13fd426f0f528a57c2aa0096289e
Siul <103662328+Siul49@users.noreply.github.com>
2025-06-09 15:42:28 +0900

    고자프 프로젝트

---
+++ gonggongjea/src/main/java/com/example/gonggongjea/GonggongjeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gonggongjea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggongjeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonggongjeaApplication.class, args);
+    }
+
+}
---
+++ gonggongjea/src/test/java/com/example/gonggongjea/GonggongjeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gonggongjea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggongjeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
